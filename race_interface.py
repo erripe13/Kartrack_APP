0c96d98244c211bdaa05aa63c6f99fc61daf7869
amalraj78 <amalraj-jcv@outlook.com>
2025-04-03 14:46:05 +0200

    update race_interface.py

--- race_interface.py
+++ race_interface.py
@@ -7,13 +7,44 @@ import tkinter as tk
 class RaceInterface:
     def __init__(self, canvas, show_main_menu):
         self.canvas = canvas
+        self.canvas.pack(fill="both", expand=True)
         self.show_main_menu = show_main_menu
         self.widgets = []
 
-        # Récupère la taille de l'écran
+        self.entry_frame = ctk.CTkFrame(
+            self.canvas,
+            width=500,  # Largeur du cadre (par exemple, 500 pixels)
+            height=300,  # Hauteur du cadre (par exemple, 300 pixels)
+            corner_radius=10,
+            fg_color="#222222"
+        )
+        # Placer le cadre au centre de l'écran
+        self.entry_frame.place(relx=0.5, rely=0.5, anchor="center")
+
+        self.entry_lat = ctk.CTkEntry(self.entry_frame, placeholder_text="Latitude",font=("Orbitron",24))
+        self.entry_lat.pack(pady=10, padx=20)
+
+        self.entry_long = ctk.CTkEntry(self.entry_frame, placeholder_text="Longitude",font=("Orbitron",24))
+        self.entry_long.pack(pady=10, padx=20,)
+
+
+        self.entry_butt = ctk.CTkButton(
+            self.entry_frame,
+            text="Valider les Coordonnées",
+            command=self.display_main_interface,
+            font=("Orbitron", 24)
+        )
+        self.entry_butt.pack(pady=20)
+
         self.screen_width = self.canvas.winfo_screenwidth()
         self.screen_height = self.canvas.winfo_screenheight()
 
+        self.map_frame = None
+        self.time_frame = None
+        self.banner = None
+
+
+    def display_main_interface(self):
         # --------- Bandeau supérieur ---------
         self.banner_height = int(self.screen_height * 0.1)
 
@@ -24,7 +55,7 @@ class RaceInterface:
             fg_color="#333333",
             corner_radius=0
         )
-        self.banner.place(x=0, y=0)
+        self.banner.place(x=0, y=0, anchor="nw")
         self.widgets.append(self.banner)
 
         self.track_name_label = ctk.CTkLabel(
@@ -39,7 +70,7 @@ class RaceInterface:
 
         # --------- Image satellite à gauche (2/3) ---------
         left_width = int(self.screen_width * 2 / 3)
-        left_height = self.screen_height - self.banner_height-50
+        left_height = self.screen_height - self.banner_height
 
         self.map_frame = ctk.CTkFrame(
             self.canvas,
@@ -48,40 +79,16 @@ class RaceInterface:
             corner_radius=0,
             fg_color="#222222"
         )
-        self.map_frame.place(x=0, y=self.banner_height+50)
+        self.map_frame.place(x=0, y=self.banner_height)
         self.widgets.append(self.map_frame)
-
         self.map_widget = tkintermapview.TkinterMapView(self.map_frame,width=(left_width-40),height=(left_height-50))
         self.map_widget.place(relx=0.5, rely=0.5, anchor="center")
-
-        # --------- Frame coordonées ---------7
-
-        entry_width = int(self.screen_width * 2 / 3)
-        entry_height = 50;
-
-        self.entry_frame = ctk.CTkFrame(
-            self.canvas,
-            width=entry_width,
-            height=entry_height,
-            corner_radius=0,
-            fg_color="#222222"
-        )
-
-        self.entry_frame.place(x=0, y=self.banner_height)
-        self.widgets.append(self.entry_frame)
-
-        self.entry_lat = ctk.CTkEntry(master=self.entry_frame, placeholder_text="lattitude :")
-        self.entry_long = ctk.CTkEntry(master=self.entry_frame, placeholder_text="longitude :")
-        self.entry_lat.grid(row=0, column=0, sticky="we", padx=(12, 0), pady=12)
-        self.entry_long.grid(row=0, column=1, sticky="we", padx=(12, 0), pady=12)
-
-        self.entry_butt = ctk.CTkButton(self.entry_frame,text="Search",width=90,command=self.search_event)
-        self.entry_butt.grid(row=0,column=3,sticky="we",padx=(12,0),pady=12)
+        self.search_event()
 
 
         # --------- Chronos à droite (1/3) ---------
         right_width = self.screen_width - left_width
-        right_height = left_height
+        right_height = self.screen_height - self.banner_height
 
         self.time_frame = ctk.CTkFrame(
             self.canvas,
@@ -90,19 +97,16 @@ class RaceInterface:
             corner_radius=0,
             fg_color="#1A1D21"
         )
-        self.time_frame.place(x=left_width, y=self.banner_height)
+        self.time_frame.place(x=left_width, y=self.banner_height, anchor="nw")
         self.widgets.append(self.time_frame)
-
-
-        self.time_label.pack(pady=20)
 
         # --------- Bouton Retour ---------
         self.button_back = ctk.CTkButton(
             self.canvas,
             text="Retour au Menu Principal",
             command=self.return_to_menu,
-            width=200,
-            height=40,
+            width=right_width,
+            height=50,
             border_width=3,
             border_color="#191919",
             fg_color=("#DB3E39", "#821D1A"),
@@ -111,7 +115,7 @@ class RaceInterface:
             text_color="white",
             font=("Orbitron", 16)
         )
-        self.button_back.place(relx=0.9, rely=0.95, anchor="center")
+        self.button_back.place(x=left_width, y=self.screen_height-50,anchor="nw")
         self.widgets.append(self.button_back)
 
         print("Interface Kartrack LIVE affichée !")
@@ -122,4 +126,6 @@ class RaceInterface:
     def return_to_menu(self):
         for widget in self.widgets:
             widget.destroy()
+
+        self.entry_frame.destroy()
         self.show_main_menu()
