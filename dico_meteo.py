3fdf962f13036c9d1f764e05ddb885bab7d23fc3
amalraj78 <amalraj-jcv@outlook.com>
2025-04-03 17:10:45 +0200

    ajout biblio meteo/test affichage meteo

---
+++ dico_meteo.py
@@ -0,0 +1,15 @@
+# Chemin dossier GIFs
+GIF_FOLDER = "meteo_icons/"
+
+# Association entre condition météo et fichier GIF
+CONDITIONS_TO_GIFS = {
+    "Ensoleillé": "icons8-soleil.gif",
+    "Nuageux": "icons8-nuageux.gif",
+    "Pluie légère": "icons8-pluie.gif",
+    "Orage": "icons8-eclair.gif",
+    "Neige": "icons8-neige.gif",
+    "Bruine": "icons8-pluie-moderee.gif",
+    "Partiellement nuageux": "icons8-partly_cloudy.gif",
+    "Couvert": "icons8-risques-de-tempete.gif",
+    # Ajoutez d'autres correspondances selon vos besoins
+}
