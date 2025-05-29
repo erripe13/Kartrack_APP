2416f171b40c615256fd57fa60834886aa2e9ae5
amalraj78 <amalraj-jcv@outlook.com>
2025-05-29 12:01:18 +0200

    modif météo

--- dico_meteo.py
+++ dico_meteo.py
@@ -1,14 +1,13 @@
 # Chemin dossier GIFs
-GIF_FOLDER = "meteo_icons/"
+IMG_FOLDER = "meteo_icons/"
 
 # Association entre condition météo et fichier GIF
-CONDITIONS_TO_GIFS = {
+CONDITIONS_TO_IMG = {
     "Ensoleillé": "icons8-soleil-64.png",
     "Nuageux": "icons8-nuage-64.png",
     "Pluie légère": "icons8-peu-nuageux-64.png",
     "Orage": "icons8-eclair-64.png",
     "Neige": "icons8-neige-64.png",
-    "Partiellement nuageux": "icons8-partiellement_nuageuse-64.png",
+    "Partiellement nuageux": "icons8-partiellement-nuageuse-64.png",
     "Couvert": "icons8-partiellement-nuageux-avec-pluie-64.png",
-    # Ajoutez d'autres correspondances selon vos besoins
-}
+    }
